710be4a7caf82c412bafc392ac0a976ea077dc17
Eddie Shen <eddie880509@gmail.com>
2023-12-09 14:15:47 +0800

    further refactor

--- baseline.py
+++ baseline.py
@@ -7,107 +7,15 @@ import torch
 import numpy as np
 import torchvision.transforms as transforms
 import matplotlib
-import matplotlib.pyplot as plt
 import PIL.Image as pil
 from easydict import EasyDict
-from scipy.spatial.transform import Rotation
 
 from tools.load import LoadMADSData
-from tools.common import project_3d_to_2d, get_projection_matrix, triangulation
-from tools.utils import get_max_preds
+from tools.common import get_projection_matrix, triangulation
+from tools.utils import get_max_preds, project, plot_pose_2d, plot_pose_3d
 from models.poseresnet import PoseResNet
 
 matplotlib.use("Agg")
-
-
-def project(meta):
-    K_left = meta['cam_left']['intrinsics']
-    R_left = meta['cam_left']['rotation']
-    T_left = meta['cam_left']['translation']
-
-    K_right = meta['cam_right']['intrinsics']
-    R_right = meta['cam_right']['rotation']
-    T_right = meta['cam_right']['translation']
-
-    pose_3d = np.array(meta['pose_3d'])
-    pose_2d_left = project_3d_to_2d(pose_3d, K_left, R_left, T_left)
-    pose_2d_right = project_3d_to_2d(pose_3d, K_right, R_right, T_right)
-
-    return pose_2d_left, pose_2d_right
-
-
-def plot_body(ax, points, color, label):
-    # Define the connections between the joints
-    connections = [
-        (0, 1),   # body
-        (0, 18),  # head
-        (1, 6), (6, 7), (7, 8), (8, 9),  # left leg
-        (0, 14), (14, 15), (15, 16), (16, 17),  # left arm
-        (1, 2), (2, 3), (3, 4), (4, 5),  # right leg
-        (0, 10), (10, 11), (11, 12), (12, 13),  # right arm
-    ]
-
-    # Plot the skeleton joints
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2],
-               c=color, marker='o', s=2)
-
-    for connection in connections:
-        joint1 = points[connection[0]]
-        joint2 = points[connection[1]]
-        ax.plot([joint1[0], joint2[0]],
-                [joint1[1], joint2[1]],
-                [joint1[2], joint2[2]], c=color)
-
-    ax.plot([], [], c=color, label=label)
-
-
-def plot_pose_3d(pose_3d, pts3D):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlim3d(-600, 600)
-    ax.set_ylim3d(-1000, 200)
-    ax.set_zlim3d(0, 1500)
-
-    rot = Rotation.from_euler('zyx', np.array([0, 0, 90]),
-                              degrees=True).as_matrix()
-    pose_3d = (rot @ pose_3d.T).T
-    pts3D = (rot @ pts3D.T).T
-
-    plot_body(ax, pose_3d, '#03459c', "ground truth")
-    plot_body(ax, pts3D, '#27d128', "estimation")
-
-    # Set labels and title
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.set_title('3D Human Skeleton')
-    ax.legend()
-
-    # Convert the plot to numpy array
-    canvas = fig.canvas
-    canvas.draw()
-    width, height = canvas.get_width_height()
-    image_array = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
-    image_array = image_array.reshape(height, width, 3)
-    plt.close()
-
-    return image_array
-
-
-def plot_pose_2d(gt_joints, pred_joints, imgs):
-    def plot_joints(joints, img, color):
-        for k in range(joints.shape[0]):
-            joint = joints[k]
-            cv2.circle(img, (int(joint[0]), int(joint[1])),
-                       2, color, -1)
-
-    for gt, pred, img in zip(gt_joints, pred_joints, imgs):
-        plot_joints(gt, img, (255, 0, 0))
-        plot_joints(pred, img, (0, 255, 0))
-
-    img = np.concatenate(imgs, axis=1)
-
-    return img
 
 
 class BaseLine:
